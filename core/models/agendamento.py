88f2bc6c70560653fceb8f67bef92b88bf9cd2a5
Rafabitte17 <rafabitte1@gmail.com>
2025-08-12 14:49:33 -0300

    Feat: Fazendo as chaves estrangeiras

--- core/models/agendamento.py
+++ core/models/agendamento.py
@@ -1,14 +1,11 @@
 from django.db import models
-
-from core.models import veterinario
-
+from core.models import Veterinario, Tutor
 
 class Agendamento(models.Model):
     data_hora = models.DateTimeField()
     status = models.CharField(max_length=45)
-    tutor = models.ForeignKey(on_delete=models.PROTECT, related_name='tutor', null=True, blank=True)
-    veterinario = models.ForeignKey(on_delete=models.PROTECT, related_name='veterinario', null=True, blank=True)
+    tutor = models.ForeignKey(Tutor, on_delete=models.PROTECT, related_name='tutor', null=True, blank=True)
+    veterinario = models.ForeignKey(Veterinario, on_delete=models.PROTECT, related_name='veterinario', null=True, blank=True)
 
-
-def __str__(self):
-    return f'({self.id}) {self.data_hora} ({self.status})'
+    def __str__(self):
+        return f'({self.id}) {self.data_hora} ({self.status})'
--- core/models/servico.py
+++ core/models/servico.py
@@ -1,9 +1,10 @@
 from django.db import models
 
+
 class Servico(models.Model):
     nome = models.CharField(max_length=45)
     descricao = models.CharField(max_length=255)
-    tutor = models.ForeignKey(on_delete=models.PROTECT, related_name='tutor', null=True, blank=True)
+    tutor = models.ForeignKey('core.Tutor', on_delete=models.PROTECT, related_name='servicos', null=True, blank=True)
 
     def __str__(self):
         return f'({self.id}) {self.nome} ({self.descricao})'
